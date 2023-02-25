4ffaaa72df421cfd93d26427258c7fa8cd254463
z0r0z <zrzr8963@gmail.com>
2023-02-25 16:11:56 +0800

    ✍️ Expand around testing

--- src/VirtualSafeToken.sol
+++ src/VirtualSafeToken.sol
@@ -8,22 +8,21 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 /// @author z0r0z.eth
 /// @custom:coauthor ellie.lens
 /// @notice Makes Safe Token opt-in transferable through tx guard.
-/// Users can mint vSAFE by locking their SAFE while it is paused.
+/// Users can mint vSAFE equal to their SAFE while it is paused.
 /// @dev Includes improvements such as ERC2612 and gas golfing.
 contract VirtualSafeToken is BaseGuard, ERC20("Virtual Safe Token", "vSAFE", 18) {
     /// @dev Canonical deployment of Safe Token on Ethereum.
     address internal constant safeToken = 0x5aFE3855358E112B5647B952709E6165e1c1eEEe;
 
-    /// @dev Tracks claims by Safes.
+    /// @dev Internal flag to ensure this guard is enabled.
+    uint256 internal guardCheck = 1;
+
+    /// @dev Tracks mint claims by Safes.
     mapping( /*safe*/ address => /*mint*/ bool) public minted;
 
     /// @dev We can cut 10 opcodes in the creation-time
     /// EVM bytecode by declaring constructor payable.
     constructor() payable {}
-
-    /// @dev We don't revert on fallback to avoid issues in case of a Safe upgrade -
-    /// e.g., the expected check method might change and then the Safe would be locked.
-    fallback() external {}
 
     /// @dev Fetches whether Safe Token is paused.
     function paused() public view returns (bool) {
@@ -31,14 +30,29 @@ contract VirtualSafeToken is BaseGuard, ERC20("Virtual Safe Token", "vSAFE", 18)
     }
 
     /// @dev Mints unclaimed vSAFE to SAFE holders.
-    function mint() external payable {
-        if (!minted[msg.sender]) {
+    function mint() public payable {
+        // Ensure this call is by guarded Safe.
+        require(guardCheck == 2, "Unguarded.");
+
+        // Reset guard value.
+        guardCheck = 1;
+
+        // Ensure no mint during transferable period.
+        require(paused(), "Unpaused.");
+
+        // Ensure no double mint and mint balance of Safe Token.
+        if (minted[msg.sender] = true == !minted[msg.sender]) {
             _mint(msg.sender, ERC20(safeToken).balanceOf(msg.sender));
         }
     }
 
+    /// @dev Burn an amount of vSAFE.
+    function burn(uint256 amount) public payable {
+        _burn(msg.sender, amount);
+    }
+
     /// @dev Called by the Safe contract before a transaction is executed.
-    /// Reverts if the transaction comes from a Safe after token unlock.
+    /// Reverts if the transaction is to a Safe during Safe Token lock.
     /// @param to Destination address of the Safe transaction.
     function checkTransaction(
         address to,
@@ -54,16 +68,19 @@ contract VirtualSafeToken is BaseGuard, ERC20("Virtual Safe Token", "vSAFE", 18)
         address
     )
         external
-        view
         override
     {
+        // Ensure mint by guarded Safe.
         if (to == address(this)) {
-            require(paused(), "This call is restricted");
-        } else {
-            require(to != address(safeToken), "This call is restricted");
+            guardCheck = 2;
+        } else if (to == msg.sender) {
+            // If callback to Safe,
+            // prevent disabling guard
+            // while Safe Token is locked.
+            require(!paused(), "Paused.");
         }
     }
 
-    /// @dev Placeholder to receive after-execution check from a Safe.
+    /// @dev Placeholder for after-execution check in Safe guard.
     function checkAfterExecution(bytes32, bool) external view override {}
 }
--- test/VirtualSafeToken.t.sol
+++ test/VirtualSafeToken.t.sol
@@ -4,18 +4,72 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 
 import {VirtualSafeToken} from "src/VirtualSafeToken.sol";
+import {Safe} from "lib/safe-contracts/contracts/Safe.sol";
+import {SafeToken} from "lib/safe-token/contracts/SafeToken.sol";
 
 contract VirtualSafeTokenTest is Test {
     using stdStorage for StdStorage;
 
-    VirtualSafeToken token;
+    VirtualSafeToken internal token;
+
+    Safe internal constant safeTreasury = Safe(payable(0x8CF60B289f8d31F737049B590b5E4285Ff0Bd1D1));
+    SafeToken internal constant safeToken = SafeToken(0x5aFE3855358E112B5647B952709E6165e1c1eEEe);
+    uint256 internal constant safeTokenAmount = 1181.539172890329705987 * 1 ether;
 
     function setUp() public payable {
+        // Create Ethereum fork.
+        vm.createSelectFork(vm.rpcUrl("mainnet"));
+
+        // Create test vSAFE token.
         token = new VirtualSafeToken();
+
+        // Simulate setting guard to vSAFE.
+        vm.prank(address(safeTreasury));
+        safeTreasury.setGuard(address(token));
     }
 
     // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
-    // Or at https://github.com/foundry-rs/forge-std
+    // or at https://github.com/foundry-rs/forge-std
 
-    function testClaim() external {}
+    function testSafeTokenOwner() public payable {
+        // We check our simulated Safe Token foundation treasury is Safe Token owner.
+        assertEq(safeToken.owner(), address(safeTreasury));
+    }
+
+    function testSafeTokenBalance() public payable {
+        // We check Safe treasury balance of Safe Token is current.
+        assertEq(safeToken.balanceOf(address(safeTreasury)), safeTokenAmount);
+    }
+
+    function testSafeTokenPaused() public payable {
+        // We check Safe Token is paused and non-transferable.
+        assertTrue(safeToken.paused());
+    }
+    /*
+    function testMintVSafeToken() public payable {
+        // Mint virtual tokens equal to Safe Token.
+        vm.prank(address(safeTreasury));
+        token.mint();
+
+        // Confirm virtual balance is equal to Safe Token.
+        assertEq(token.balanceOf(address(safeTreasury)), safeTokenAmount);
+    }
+
+    function testCannotMintVSafeTokenTwice() public payable {
+        // Mint virtual tokens equal to Safe Token.
+        vm.prank(address(safeTreasury));
+        token.mint();
+
+        // Confirm virtual balance is equal to Safe Token.
+        assertEq(token.balanceOf(address(safeTreasury)), safeTokenAmount);
+
+        // We expect second mint to fail as already claimed.
+        vm.prank(address(safeTreasury));
+        vm.expectRevert("Already minted.");
+        token.mint();
+
+        // Confirm balance is unchanged.
+        assertEq(token.balanceOf(address(safeTreasury)), safeTokenAmount);
+    }
+    */
 }
